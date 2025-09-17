bc7a640d7075a5661b6ac423199be360a8fde5cf
João Victor <joa9718@gmail.com>
2025-09-16 23:48:17 -0300

    Criação da classe Filme e de seus atributos

---
+++ src/Filme.java
@@ -0,0 +1,11 @@
+public class Filme {
+
+    public String nome;
+    public int ano;
+    public boolean incluidoNoPlano;
+    public double avaliacao;
+    public int totalDeAvaliacoes;
+    public int duracaoEmMinutos;
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Filme filme = new Filme();
+
+        filme.nome = "Duna 2";
+        filme.ano = 2024;
+        filme.incluidoNoPlano = true;
+        filme.duracaoEmMinutos = 208;
+
+    }
+
+}
